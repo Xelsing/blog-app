249833fdd840023c47c80d1f31f69cd537dcd706
Xelsing <vano.03.06@gmail.com>
2022-12-19 14:01:45 +0300

    вывод ошибки

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import PostForm from './components/PostForm';
-/* import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
-import PostItem from './components/PostItem'; */
 import PostList from './components/PostList';
-// import MySelect from './components/UI/select/MySelect';
-// import MyButton from './components/UI/button/MyButton';
-// import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
-// import axios from 'axios';
 import PostService from './API/PostService'
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
 
 function App() {
     const [posts, setPosts] = useState([]);
     const [filter, setFilter] = useState({ sort: '', query: '' });
     const [modal, setModal] = useState(false);
+    const [totalCount, setTotalCount] = useState(0);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostsLoading, setIsPostsLoading] = useState(false);
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll();
+        setPosts(response.data);
+        setTotalCount(response.headers['x-total-count'])
+        console.log(response.headers['x-total-count']);
+    })
 
     useEffect(() => {
         fetchPosts()
@@ -30,15 +31,6 @@ function App() {
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
-    }
-
-    async function fetchPosts() {
-        setIsPostsLoading(true);
-        setTimeout(async () => {
-            const posts = await PostService.getAll();
-            setPosts(posts);
-            setIsPostsLoading(false);
-        }, 1000)
     }
 
     const removePost = (post) => {
@@ -58,6 +50,9 @@ function App() {
                 filter={filter}
                 setFilter={setFilter}
             />
+            {postError &&
+                <div><h1 style={{ textAlign: 'center' }}>Произошла ошибка</h1><h1 style={{ textAlign: 'center' }}>${postError}</h1></div>
+            }
             {isPostsLoading
                 ? <h1 style={{ textAlign: 'center' }}>Загрузка постов... <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}><Loader /></div></h1>
                 : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты бложика" />
