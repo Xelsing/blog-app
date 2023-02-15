437b1da0b94afd4b748f78580d26abecb6e32e3a
Xelsing <vano.03.06@gmail.com>
2023-02-15 13:27:24 +0300

    before change routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,12 +6,12 @@ import PostIdPage from "../pages/PostIdPage";
 
 
 export const privateRoutes = [
-    { path: '/blog-app/about', element: <About /> },
-    { path: '/blog-app/posts/', element: <Posts /> },
-    { path: '/blog-app/posts/:id', element: <PostIdPage /> },
-    { path: '/blog-app/404', element: <ErrorPage /> },
+    { path: '/about', element: <About /> },
+    { path: '/posts/', element: <Posts /> },
+    { path: '/posts/:id', element: <PostIdPage /> },
+    { path: '/404', element: <ErrorPage /> },
 ]
 
 export const publicRoutes = [
-    { path: '/blog-app/login', element: <Login /> },
+    { path: '/login', element: <Login /> },
 ]
